8cc40302a62572b54c3a9213b7ac5df42bcd7e50
SameerUH <sameerulhaq2005@gmail.com>
2026-06-15 18:24:49 +0100

    Initial commit, added basic buttons, still need to design everything.

---
+++ to-do-list/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="rowbuttons">
+      <button>Button1</button>
+      <button>Button2</button>
+      <button>Button3</button>
+      <button>Button4</button>
+    </div>
+    </>
+  )
+}
+
+export default App
