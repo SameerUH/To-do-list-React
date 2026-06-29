341ad39ee3cffea9fa169dc8a7cc257866b861b8
SameerUH <sameerulhaq2005@gmail.com>
2026-06-29 21:01:09 +0100

    Installed Tailwind and converted CSS to Tailwind

--- to-do-list/src/App.jsx
+++ to-do-list/src/App.jsx
@@ -5,15 +5,15 @@ function App() {
 
   return (
     <>
-    <div className="navbar">
-      <button>Button1</button>
-      <button>Button2</button>
-      <p>logo</p>
-      <button>Button3</button>
-      <button>Button4</button>
+    <div class="flex justify-evenly h-5 w-full mt-[1em] mb-[200px] mx-auto my-0">
+      <button class="rounded-sm padding-[2em] bg-blue-100 text-black">Button1</button>
+      <button class="rounded-sm padding-[2em] bg-blue-100 text-black">Button2</button>
+      <p class="decoration-solid sideways-lr">logo</p>
+      <button class="rounded-sm padding-[2em] bg-blue-100 text-black">Button3</button>
+      <button class="rounded-sm padding-[2em] bg-blue-100 text-black">Button4</button>
     </div>
 
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, a illo laudantium ipsa fugiat repellendus tenetur. Quisquam illo quae explicabo consectetur aliquid unde asperiores nemo dolores magnam. Impedit, vero dolorum.</p>
+    <p class="max-inline-[10ch] sideways-lr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, a illo laudantium ipsa fugiat repellendus tenetur. Quisquam illo quae explicabo consectetur aliquid unde asperiores nemo dolores magnam. Impedit, vero dolorum.</p>
     </>
   )
 }
